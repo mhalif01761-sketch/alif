1201e53596b8729be75ac2e5b1fd75ad634c7f2b
mhalif01761-sketch <mhalif01761@gmail.com>
2025-10-28 13:40:48 +0600

    Add files via upload

---
+++ Task7.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+    string first, last;
+    cin >> first >> last;
+    string full = first + " " + last;
+    cout << "Full name = " << full;
+    return 0;
+}
